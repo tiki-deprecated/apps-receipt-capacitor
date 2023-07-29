060b0cb463525329da6638e7ab83f1f6aa8afa88
Mike Audi <mike@mytiki.com>
2023-07-29 02:21:30 -0500

    feat: add link account screen
    not 100% complete. missing error state handling and null state for "no linked accounts"

---
+++ src/components/tiki/element/AccountCarousel.vue
@@ -0,0 +1,96 @@
+<!--
+  - Copyright (c) TIKI Inc.
+  - MIT license. See LICENSE file in root directory.
+  -->
+
+<script setup lang="ts">
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide } from "vue3-carousel";
+import AccountIconSolid from "@/components/tiki/icon/AccountIconSolid.vue";
+import UnlinkIcon from "@/components/tiki/icon/UnlinkIcon.vue";
+
+// defineProps({
+//   accounts: Array<{
+//     type: String;
+//     username: String;
+//   }>,
+// });
+
+const accounts = [
+  {
+    type: AccountIconSolid,
+    username: "mike@mytiki.com",
+  },
+  {
+    type: AccountIconSolid,
+    username: "mike@mytiki.com",
+  },
+  {
+    type: AccountIconSolid,
+    username: "mike@mytiki.com",
+  },
+  {
+    type: AccountIconSolid,
+    username: "mike@mytiki.com",
+  },
+];
+</script>
+
+<template>
+  <carousel :items-to-show="3.5" snapAlign="start">
+    <slide v-for="account in accounts" :key="accounts">
+      <div>
+        <div class="account-button" role="button">
+          <component :is="account.type" class="icon" />
+          <div class="unlink"><unlink-icon class="unlink-icon" /> Unlink</div>
+        </div>
+        <p class="username">{{ account.username }}</p>
+      </div>
+    </slide>
+  </carousel>
+</template>
+
+<style scoped>
+.account-button {
+  background-color: var(--tiki-primary-background-color);
+  font-size: var(--tiki-font-size);
+  border-radius: 0.625em;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 5%);
+  display: block;
+}
+
+.icon {
+  height: 2.75em;
+  margin: 1em 0 0.571em 0;
+}
+
+.unlink {
+  font-family: var(--tiki-font-family);
+  font-size: var(--tiki-font-size-sm);
+  line-height: var(--tiki-line-height-sm);
+  font-weight: bold;
+  color: var(--tiki-secondary-text-color);
+  display: flex;
+  align-items: center;
+  padding: 0 0.571em 0.571em 0.571em;
+}
+
+.unlink-icon {
+  margin-right: 0.571em;
+  height: 1.714em;
+}
+
+.username {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-family: var(--tiki-font-family);
+  font-size: var(--tiki-font-size-xs);
+  line-height: var(--tiki-line-height-xs);
+  font-weight: 500;
+  color: var(--tiki-secondary-text-color);
+  width: 7.5em;
+  display: block;
+  margin: 0.5em 0;
+}
+</style>
--- src/components/tiki/screen/Account.vue
+++ src/components/tiki/screen/Account.vue
@@ -9,7 +9,10 @@ import CloseIcon from "@/components/tiki/icon/CloseIcon.vue";
 import CircleButton from "@/components/tiki/element/CircleButton.vue";
 import { ref } from "vue";
 import BackButton from "@/components/tiki/element/BackButton.vue";
-import HistoryDetails from "@/components/tiki/element/HistoryDetails.vue";
+import AccountForm from "@/components/tiki/element/AccountForm.vue";
+import TextButton from "@/components/tiki/element/TextButton.vue";
+import AccountIconOutline from "@/components/tiki/icon/AccountIconOutline.vue";
+import AccountCarousel from "@/components/tiki/element/AccountCarousel.vue";
 
 const show = ref(true);
 </script>
@@ -21,7 +24,10 @@ const show = ref(true);
         <back-button text="Rewards" />
         <circle-button @click="show = false" :icon="CloseIcon" />
       </div>
-      <history-details />
+      <account-form />
+      <p class="linked-accounts">Linked Accounts</p>
+      <account-carousel class="account-carousel" />
+      <text-button text="Link Account" :icon="AccountIconOutline" />
     </div>
   </bottom-sheet>
 </template>
@@ -37,5 +43,19 @@ const show = ref(true);
   align-items: flex-start;
   justify-content: space-between;
   margin: 0 0.5em 2em 0.5em;
+}
+
+.linked-accounts {
+  font-family: var(--tiki-font-family);
+  font-size: var(--tiki-font-size);
+  line-height: var(--tiki-line-height);
+  font-weight: bold;
+  color: var(--tiki-primary-text-color);
+  text-transform: uppercase;
+  margin: 0;
+}
+
+.account-carousel {
+  margin-bottom: 1em;
 }
 </style>
