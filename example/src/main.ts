30fc0ba1f3ceb59b6d0ad15aa256bd303a0c90a8
Mike Audi <mike@mytiki.com>
2023-08-13 01:04:20 -0500

    fix: set default program and rewarads
    also fixes bug in joining account name text with history event name

--- example/src/main.ts
+++ example/src/main.ts
@@ -8,7 +8,10 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "@/app.vue";
 
-import ProgramCard from "@/assets/program-card.png";
+import Program from "@/assets/program.png";
+import LinkReward from "@/assets/link-reward.png";
+import ScanReward from "@/assets/scan-reward.png";
+import MoreReward from "@/assets/more-reward.png";
 import LearnMore from "@/assets/learn-more.md?raw";
 import Terms from "@/assets/terms.md?raw";
 
@@ -31,32 +34,62 @@ createApp(App)
         "wSNX3mu+YGc/2I1DDd0NmrYHS6zS1BQt2geMUH7DDowER43JGeJRUErOHVwU2tz6xHDXia8BuvXQI3j37I0uYw==",
     },
     program: {
-      image: ProgramCard,
+      image: Program,
       description:
-        "Share your receipts to earn rewards like gift cards or cash!",
+        "You can now trade YOUR data for cash! Just scan a receipt or link an account.",
       terms: Terms,
       learn: LearnMore,
       bullets: [
-        { text: "De-identified (for your privacy)", isUsed: true },
-        { text: "Develop market insights", isUsed: false },
-        { text: "Deliver a more personalized experience ", isUsed: true },
+        { text: "Creepy targeted ads", isUsed: false },
+        { text: "Spot purchasing trends", isUsed: true },
+        { text: "Create aggregate insights", isUsed: true },
       ],
-      usecases: [Usecase.common(CommonUsecases.DISTRIBUTION)],
-      tags: [Tag.common(CommonTags.PURCHASE_HISTORY)],
+      usecases: [
+        Usecase.common(CommonUsecases.DISTRIBUTION),
+        Usecase.common(CommonUsecases.ANALYTICS),
+        Usecase.common(CommonUsecases.AI_TRAINING),
+        Usecase.common(CommonUsecases.ATTRIBUTION),
+      ],
+      tags: [
+        Tag.common(CommonTags.USER_ID),
+        Tag.common(CommonTags.PURCHASE_HISTORY),
+      ],
     },
     theme: {
       accentColor: "#783F10",
     },
     rewards: [
       {
-        image: ProgramCard,
+        image: ScanReward,
         description:
-          "Share your receipts to earn rewards like gift cards or cash!",
+          "Earn 10 points for every receipt you scan or in your linked accounts.",
         issuer: (
           event: ReceiptEvent,
           details: { receipt?: Receipt; account?: ReceiptAccount },
         ): number | undefined => {
-          if (event == ReceiptEvent.SCAN) return 50;
+          if (event == ReceiptEvent.SCAN) return 10;
+        },
+      },
+      {
+        image: LinkReward,
+        description:
+          "Earn 100 points for every account you link. We only check it for receipts.",
+        issuer: (
+          event: ReceiptEvent,
+          details: { receipt?: Receipt; account?: ReceiptAccount },
+        ): number | undefined => {
+          if (event == ReceiptEvent.LINK) return 100;
+        },
+      },
+      {
+        image: MoreReward,
+        description:
+          "Check back for special offers and more ways to earn cash for your data.",
+        issuer: (
+          event: ReceiptEvent,
+          details: { receipt?: Receipt; account?: ReceiptAccount },
+        ): number | undefined => {
+          return undefined;
         },
       },
     ],
