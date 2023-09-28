076afa1e1881ac0d3de587317adbf0b862cc11fb
Miro Benicio <mirodantas27@gmail.com>
2023-09-28 12:37:11 -0300

    doc of the notification service

--- src/service/notification-service.ts
+++ src/service/notification-service.ts
@@ -1,11 +1,19 @@
 import { LocalNotifications } from '@capacitor/local-notifications';
 
 export class NotificationService {
-
+    /**
+     * Request permission from the user to show notifications
+     * It's necessary to use the sendNotification method
+     */
     authorize = async () => {
         await LocalNotifications.requestPermissions()
     }
 
+    /**
+     * Send a local notification to the user after 5 seconds 
+     * @param title - the title of the notification 
+     * @param body - the body of the notification
+     */
     sendNotification = async (title: string, body: string) => {
         await LocalNotifications.schedule({
             notifications: [
