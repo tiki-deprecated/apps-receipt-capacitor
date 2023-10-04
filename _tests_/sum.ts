fd1216e4c3d939acd1d971527c235afbe8b89480
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-10-04 11:21:06 -0300

    fix: jest setup

--- _tests_/sum.ts
+++
@@ -1,4 +0,0 @@
-export function sum(a:number, b:number): number{
-    return a + b;
-}
-
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFiles: ["<rootDir>/__tests__/__mocks__/localStorageMock.js"],
   verbose: true,
-  automock: true,
   globals: {
     crypto: {
-    getRandomValues: (arr) => require("crypto").randomBytes(arr.length),
+      getRandomValues: (arr) => require("crypto").randomBytes(arr.length),
     },
-    
   },
-};
+}
