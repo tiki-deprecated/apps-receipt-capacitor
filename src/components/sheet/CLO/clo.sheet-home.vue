7fb32a322ca3ef64802fd89ef7867b513e7da752
Miro Benicio <mirodantas27@gmail.com>
2023-10-20 16:17:13 -0300

    WIP: home sheet and card component

--- src/components/sheet/CLO/clo.sheet-home.vue
+++ src/components/sheet/CLO/clo.sheet-home.vue
@@ -2,10 +2,22 @@
 import {
   ButtonText,
   HeaderTitle,
-  CloCardOffer
+  CloCardHome
 } from "../../../components";
 </script>
 
 <template>
-    
+    <div>
+        <header-title 
+        title="Cashback Connections"
+        subtitle="Share data. Earn rewards."
+        @close="$emit('close')"    
+        /> 
+        <clo-card-home />
+        
+    </div>
 </template>
+
+<style scoped>
+
+</style>
