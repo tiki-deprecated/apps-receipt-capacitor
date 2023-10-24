81d4327365b07baa71de95424382144e56a1377d
Miro Benicio <mirodantas27@gmail.com>
2023-10-24 11:14:41 -0300

    WIP: company sheet

---
+++ src/components/sheet/CLO/clo-sheet-company.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+    HeaderBack, 
+} from "../../../components";
+import { defineProps, PropType } from "vue";
+import { AccountType } from "@mytiki/capture-receipt-capacitor";
+
+const props = defineProps({
+    company: {
+        type: Object as PropType<AccountType>,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <div>
+        <header-back @close="$emit('close')" @back="$emit('back')" :text="company.name"/>
+    </div>
+</template>
+
+<style scoped>
+</style>
--- src/components/sheet/CLO/index.ts
+++ src/components/sheet/CLO/index.ts
@@ -1,5 +1,6 @@
 import CloSheetOffer from "./clo-sheet-offer.vue";
 import CloSheetHome from "./clo.sheet-home.vue"
 import CloSheetDetails from "./clo-sheet-details.vue"
+import CloSheetCompany from "./clo-sheet-company.vue"
 
-export {CloSheetOffer, CloSheetHome, CloSheetDetails}
+export {CloSheetOffer, CloSheetHome, CloSheetDetails, CloSheetCompany}
