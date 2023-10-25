590aee440719e7e657a5eee22aa16b7f3348021a
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-10-25 17:38:08 -0300

    fix: replace jest with vitest

--- __tests__/__fixtures__/fourWeeks.ts
+++ __tests__/__fixtures__/fourWeeks.ts
@@ -1,6 +1,5 @@
 import { StateSync } from "../../src/service/store/state";
 export default function fourWeeks (stateSync: StateSync): number{
-    // add the 4 weeks logins
     stateSync.add(new Date('09/05/2023'))
     stateSync.add(new Date('09/12/2023'))
     stateSync.add(new Date('09/19/2023'))
--- __tests__/allRequirements.test.ts
+++ __tests__/allRequirements.test.ts
@@ -1,31 +1,30 @@
 import { StateAccount } from "../src/service/store/state/state-account";
-import { ServiceStore } from "../src/service/store";
+import { Store } from "../src/service/store";
 import connectGmail from "./__fixtures__/connectGmail";
 import connectRetailer from "./__fixtures__/connectRetailer";
 import fiveReceipts from "./__fixtures__/fiveReceipts";
 import fourWeeks from "./__fixtures__/fourWeeks";
 import mockCheckPayout from "./__mocks__/mockCheckPayout";
+import {test, expect} from "vitest";
 
-describe("All Requirements Fullfilled", () => {
-  test("Test suite", () => {
-    const store = new ServiceStore();
-    const gmail = store.gmail as StateAccount;
-    expect(connectGmail(gmail)).toBe("P100");
+test("All Requirements Fullfilled", () => {
+  const store = new Store();
+  const gmail = store.gmail as StateAccount;
+  expect(connectGmail(gmail)).toBe("P100");
 
-    const retailer = store.retailer as StateAccount;
-    expect(connectRetailer(retailer)).toBe("P100");
+  const retailer = store.retailer as StateAccount;
+  expect(connectRetailer(retailer)).toBe("P100");
 
-    expect(fourWeeks(store.sync)).toBe(4);
+  expect(fourWeeks(store.sync)).toBe(4);
 
-    expect(fiveReceipts(store.receipt)).toBe(5);
+  expect(fiveReceipts(store.receipt)).toBe(5);
 
-    expect(
-      mockCheckPayout(
-        store.sync.countWeeks(new Date("09/27/2023")),
-        store.receipt.count(),
-        gmail.get().value,
-        retailer.get().value
-      )
-    ).toBeTruthy();
-  });
+  expect(
+    mockCheckPayout(
+      store.sync.countWeeks(new Date("09/27/2023")),
+      store.receipt.count(),
+      gmail.get().value,
+      retailer.get().value
+    )
+  ).toBeTruthy();
 });
